055abb25cc1320079bdd8fcb3d3c7209afcb4b5b
nrlain0506 <59519883+nrlain0506@users.noreply.github.com>
2020-01-05 02:31:35 +0000

    u
    mzmz

--- src/userinfo.py
+++ src/userinfo.py
@@ -11,7 +11,8 @@ class UserInfo:
     '''
     user_agent = ("Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 "
                   "(KHTML, like Gecko) Chrome/48.0.2564.103 Safari/537.36")
-    url_user_info = "https://www.instagram.com/%s/"
+    url_user_info = "https://www.instagram.com/nrlain0506
+/"
     url_list = {
         "ink361": {
             "main": "http://ink361.com/",
